4604610092097c44fd64e1e8a2d5d94224475342
devKD <hiakashkd@gmail.com>
2023-02-17 00:59:25 +0530

    App live : version: 1.1.46+36

--- lib/util/app_constant.dart
+++ lib/util/app_constant.dart
@@ -4,8 +4,7 @@ class AppConstant{
  static const liveBaseUrl = "https://apiv2app.spayindia.in/AppSpayService.svc/";
  static const uatBaseUrl = "https://api.spayindia.in/AppSpayService.svc/";
 
- //todo remove in production (uatBaseUrl)
- static String baseUrl = uatBaseUrl;
+ static String baseUrl = liveBaseUrl;
 
  static const profileBaseUrl = "https://spayindia.in/commonimg/user/";
  static const requestImageBaseUrl = "https://spayindia.in/commonimg/request/";
